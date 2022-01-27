8a4e40135de0191b0a9212bde237df9800951db1
Nour Rabea <75251467+Nour-rabea@users.noreply.github.com>
2022-01-27 12:21:01 -0800

    Update map.js

--- scripts/map.js
+++ scripts/map.js
@@ -520,7 +520,7 @@ $(window).on('load', function() {
         color: tryPolygonSetting(polygon, '_outlineColor', 'white'),
         dashArray: '3',
         fillOpacity: tryPolygonSetting(polygon, '_colorOpacity', '0.7'),
-        fill: feature.properties['img']
+        fill: feature(properties['img'])
       }
     }
   }
